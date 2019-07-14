3c1b3fbb44145de9dbbd727e9b9d35de0617e14a
David Lopez <lopezd@wustl.edu>
2019-07-14 14:57:42 -0400

    Update schema to include blue/red squares and send them to client when game is created.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const blueTeam = 'blue';
+const redTeam = 'red';
+
 let dlonamesWords;
 fs.readFile(path.resolve('../backend/static/base-words.txt'), 'utf8', (err, data) => {
     if (err) console.log(err);
@@ -10,24 +13,40 @@ fs.readFile(path.resolve('../backend/static/base-words.txt'), 'utf8', (err, data
  
 
 const getRandomTeam = () => {
-    return Math.random() < .5 ? 'blue' : 'red';
+    return Math.random() < .5 ? blueTeam : redTeam;
 }
 
 
 const getDlonamesWords = () => {
     const usedNumbers = new Set();
     return Array(25).fill().map(() => {
-        let index = Math.round(Math.random() * (dlonamesWords.length - 1));
+        let index = Math.round(Math.random() * (dlonamesWords.length));
         while (usedNumbers.has(index)) {
-            index = Math.round(Math.random() * (dlonamesWords.length - 1));
+            index = Math.round(Math.random() * (dlonamesWords.length));
         }
         usedNumbers.add(index);
         return dlonamesWords[index].toLowerCase();
     });
 }
 
+const getDlonamesIndices = () => {
+    const usedNumbers = new Set();
+    return Array(18).fill().map(() => {
+        let index = Math.round(Math.random() * 24);
+        while (usedNumbers.has(index)) {
+            index = Math.round(Math.random() * 24);
+        }
+        usedNumbers.add(index);
+        return index;
+    });}
+
 const Mutation = {
     async createGame(parent, args, ctx, info) {
+        const indices = getDlonamesIndices();
+        const firstTeam = getRandomTeam();
+        const blueWords = firstTeam === blueTeam ? indices.slice(0, 9) : indices.slice(9, 17);
+        const redWords = firstTeam === redTeam ? indices.slice(0, 9) : indices.slice(9, 17);
+        const deathWord = indices[17];
         const game = await ctx.db.mutation.createDlonamesGame(
         {
             data: {
@@ -52,6 +71,13 @@ const Mutation = {
                 words: {
                     set: getDlonamesWords()
                 },
+                blueWords: {
+                    set: blueWords
+                },
+                redWords: {
+                    set: redWords
+                },
+                deathWord: deathWord,
             },
         },
         info
