7c95570f6585561f7d0ef164974b6c3f1c7f08d1
David Lopez <lopezd@wustl.edu>
2019-08-11 18:49:56 -0400

    Add logout link to Account Page

--- frontend/src/pages/account.js
+++ frontend/src/pages/account.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Router } from "@reach/router"
-import { login, isAuthenticated, getProfile } from "../utils/auth"
+import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
 
 const Home = ({ user }) => {
@@ -23,6 +23,15 @@ const Account = () => {
         <Link to="/account/">Home</Link>{" "}
         <Link to="/account/settings/">Settings</Link>{" "}
         <Link to="/account/billing/">Billing</Link>{" "}
+        <a
+          href="#logout"
+          onClick={e => {
+            logout()
+            e.preventDefault()
+          }}
+        >
+          Log Out
+        </a>
       </nav>
       <Router>
         <Home path="/account/" user={user} />
