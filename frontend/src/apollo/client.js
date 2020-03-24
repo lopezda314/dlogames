ef4e7e36efca735e03230d2d49f4dcff00673f5e
David Lopez <lopezd@wustl.edu>
2020-03-24 00:03:22 -0400

    Change yoga URL to be pointed to prod.

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -2,6 +2,9 @@ import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4444",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4444"
+      : "https://dlogames-yoga-prod.herokuapp.com/",
   fetch,
 })
